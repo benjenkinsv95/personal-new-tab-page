bfe572bad62f0425a5edc161fc182885daa8a224
benjenkinsv95 <benjenkinsv95@gmail.com>
2021-08-21 22:48:39 -0500

    Update to new template

--- src/index.js
+++ src/index.js
@@ -3,12 +3,12 @@ import ReactDOM from 'react-dom'
 import './index.scss'
 
 import App from './App'
-import { HashRouter } from 'react-router-dom'
+import { BrowserRouter } from 'react-router-dom'
 
 const appJsx = (
-  <HashRouter>
-    <App />
-  </HashRouter>
+	<BrowserRouter basename={process.env.PUBLIC_URL}>
+		<App />
+	</BrowserRouter>
 )
 
 ReactDOM.render(appJsx, document.getElementById('root'))
